2009257ba09d0dbd902d6a01bd9db145dd23bf84
James Dong <45552485+Dongongngg@users.noreply.github.com>
2021-05-02 13:58:25 +1000

    Page/index style update  (#1) (#2)
    * feat(page update)
    
    * fix(mui): document
    
    * fix(heading tags)
    
    Co-authored-by: James Dong <79146695+dongongngg-forexco@users.noreply.github.com>
    
    Co-authored-by: James Dong <79146695+dongongngg-forexco@users.noreply.github.com>

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import Head from 'next/head';
+//  theme
+import { CssBaseline } from '@material-ui/core';
+import { ThemeProvider } from '@material-ui/core/styles';
+import theme from '../styles/theme';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Jingfu Dong's portfolio</title>
+        <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width' />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
